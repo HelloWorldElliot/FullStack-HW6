69d734410e50e373271f4bd22a8e26c8e33cd504
Minghao Zhou <mz2962@columbia.edu>
2024-04-11 13:07:57 -0400

    Finish

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+import fs from 'fs';
+import qr from 'qr-image';
+
+async function promptForURL() {
+  const inquirer = await import('inquirer');
+  const answers = await inquirer.default.prompt([{
+    type: 'input',
+    name: 'url',
+    message: 'Enter the URL you want to generate a QR code for:',
+    validate: function(value) {
+      return value.startsWith('http://') || value.startsWith('https://') ? true : 'Please enter a valid URL.';
+    }
+  }]);
+  return answers.url;
+}
+
+function generateQRCode(url) {
+  const qrSVG = qr.image(url, { type: 'png' });
+  qrSVG.pipe(fs.createWriteStream('qr_img.png'));
+  console.log('QR code generated successfully and saved as qr_img.png!');
+}
+
+function saveURLToFile(url) {
+  fs.writeFile('URL.txt', url, (err) => {
+    if (err) throw err;
+    console.log('URL saved to URL.txt');
+  });
+}
+
+async function main() {
+  try {
+    const url = await promptForURL();
+    generateQRCode(url);
+    saveURLToFile(url);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
